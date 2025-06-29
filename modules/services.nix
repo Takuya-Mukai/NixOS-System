149769ef9704acd4df82865f639223891cd0d80d
Takuya-Mukai <takuya.mukai@gmail.com>
2025-06-29 10:47:36 +0900

    missing };

--- modules/services.nix
+++ modules/services.nix
@@ -3,10 +3,11 @@
     blueman.enable = true;
     services.polkit-gnome.enable = true;
 
-  services.tailscale.enable = true;
-  networking.firewall = {
-    enable = true;
-    trustedInterfaces = ["tailscale0"];
-    allowedUDPPorts = [config.services.tailscale.port];
+    tailscale.enable = true;
+    networking.firewall = {
+      enable = true;
+      trustedInterfaces = ["tailscale0"];
+      allowedUDPPorts = [config.services.tailscale.port];
+    };
   };
 }
