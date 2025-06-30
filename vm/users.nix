9670b3d31fd8c95e88888b623020e26f7e05b73c
Takuya-Mukai <takuya.mukai@gmail.com>
2025-06-30 22:18:53 +0900

    user settings

--- vm/users.nix
+++ vm/users.nix
@@ -1,18 +1,18 @@
-# /etc/nixos/modules/users.nix
 { config, pkgs, inputs, ... }:
 
 {
-  # ユーザーの定義
   users.users.user = {
     isNormalUser = true;
     description = "user";
     extraGroups = [ "networkmanager" "wheel" "docker" "libvirt" ];
-    shell = pkgs.zsh; # デフォルトシェルなど
+    shell = pkgs.zsh;
   };
 
-  # ここでは user ユーザーに Home Manager を使うことだけを伝える
-  # 具体的な設定は、ユーザー自身のFlakeで管理される
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.users.user = { home.stateVersion = "25.05"};
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.user = {
+      home.stateVersion = "25.05";
+    };
+  };
 }
