52be61d642c786e6a7683493c4a7e73023e0f829
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-03 17:38:49 +0900

    removed opengl

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,8 +1,4 @@
 { config, pkgs, inputs, ...}:
 {
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.graphics.enable = true;
   programs.hyprland.enable = true;
 }
