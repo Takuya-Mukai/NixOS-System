1e3ff584691501297defb815403654cb9bff28b7
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-04 02:11:38 +0900

    moved zsh to nix

--- users/common.nix
+++ users/common.nix
@@ -56,21 +56,98 @@
       enable = true;
       shellAliases = { ll = "ls -l"; };
       # ...
-    };
+      history = {
+        size = 1000;
+        path = "${config.xdg.dataHome}/zsh/history";
+        save = 1000;
+      };
+
+      # エイリアス
+      shellAliases = {
+        grep = "grep --color=auto";
+        # ezaのエイリアス群
+        ei = "eza --icons --git --group-directories-first --sort=type";
+        ea = "eza -a --icons --git --group-directories-first --sort=type";
+        ee = "eza -aahl --icons --git --group-directories-first --sort=type";
+        ls = "ei";
+        la = "ea";
+        ll = "ee";
+        l  = "clear && ls";
+      };
+
+      # zinitの代わりにHome Managerのプラグイン機能を使う
+      plugins = [
+        # Pureテーマ
+        {
+          name = "pure";
+          src = pkgs.zsh-pure;
+          file = "share/zsh/site-functions/prompt_pure_setup";
+        }
+        # Oh My Zsh の git プラグイン
+        {
+          name = "oh-my-zsh-git";
+          src = pkgs.oh-my-zsh;
+          file = "plugins/git/git.plugin.zsh";
+        }
+        # zsh-users のプラグイン
+        { name = "zsh-completions"; src = pkgs.zsh-completions; }
+        { name = "zsh-history-substring-search"; src = pkgs.zsh-history-substring-search; }
+        { name = "zsh-syntax-highlighting"; src = pkgs.zsh-syntax-highlighting; }
+        { name = "zsh-autosuggestions"; src = pkgs.zsh-autosuggestions; }
+      ];
+
+      # その他のカスタム設定
+      initExtra = ''
+        # compinstallによる設定 (多くはデフォルトですが明示的に記述)
+        autoload -Uz compinit && compinit
+        zstyle ':completion:*' matcher-list 'm:{[:lower:]}={[:upper:]} r:|[._-]=** r:|=**' 'l:|=* r:|=*'
+        zstyle ':completion:*' menu select=1
+        if [ -x /usr/bin/dircolors ]; then
+            eval "$(dircolors -b)"
+        fi
+        zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
+
+        # zsh-newuser-installによる設定
+        setopt autocd extendedglob nomatch notify
+        unsetopt beep
+
+        # yaziをcd機能付きで呼び出す関数
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            builtin cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+        }
+
+        # history-substring-searchのキーバインド
+        bindkey -M vicmd 'k' history-substring-search-up
+        bindkey -M vicmd 'j' history-substring-search-down
+      '';
+      };
     neovim.enable = true; # nvimパッケージもインストールされる
   };
+  home.sessionVariables = {
+    BROWSER = "zen-browser"; # zen-browserは別途インストールが必要
+    EDITOR = "nvim";
+    TERMINAL = "foot";       # footは別途インストールが必要
+    XMODIFIERS = "@im=fcitx";
+    QT_IM_MODULE = "fcitx";
+    QT_QPA_PLATFORMTHEME = "qt6ct";
+    DefaultIMModule = "fcitx";
+  };
 
-  home.file.".zshrc" = {
-    source = ./modules/zsh/.zshrc;
-    force = true;
-  };
-  home.file.".zshrc.zni" = {
-    source = ./modules/zsh/.zshrc.zni;
-    force = true;
-  };
-  home.file.".config/nvim" = {
-    source = ./modules/config/nvim;
-    force = true;
-    recursive = true;
+  # PATHの追加
+  home.sessionPath = [
+    "$HOME/.local/bin"
+    "$HOME/.cargo/bin"
+  ];
+
+  programs.zsh = {
+    enable = true;
+    viMode = true; # bindkey -v
+
+    # .zsh_historyの設定
   };
 }
