6d44a205fc3f63144d7c74b9c3f6da2970ce1e28
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-03 19:41:59 +0900

    removed hardware-config

--- m75q/configuration.nix
+++ m75q/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      # ./hardware-configuration.nix
       ./hardware.nix
       ./users.nix
 
