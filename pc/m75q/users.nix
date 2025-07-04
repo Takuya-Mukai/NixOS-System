d2eb8175251f73f2fda7d9bfb48f0e592743b959
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-04 20:31:29 +0900

    modified

--- pc/m75q/users.nix
+++ pc/m75q/users.nix
@@ -17,7 +17,6 @@
       home.stateVersion = "25.05";
       home.username = "user";
       home.homeDirectory = "/home/user";
-      home.stateVersion = "25.05";
       imports = [
         ../../modules/cli.nix
         ../../modules/gui.nix
