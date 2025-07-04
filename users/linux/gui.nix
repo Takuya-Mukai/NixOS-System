79a8e17d6b7897fa9e713d856797acb79f00949d
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-04 17:33:06 +0900

    modified

--- users/linux/gui.nix
+++ users/linux/gui.nix
@@ -1,6 +1,7 @@
 # install obsidian and chromium with home-manager
 { config, pkgs, inputs, ... }:
 {
+  programs.chromium.enable = true;
   home.packages = with pkgs; [
     obsidian
     inputs.zen-browser.packages.${system}.specific
@@ -42,5 +43,4 @@
   # Enable the GUI applications to run in the home-manager environment
   xdg.enable = true;
   # Optional: Set up a desktop entry for Obsidian
-  programs.chromium.enable = true;
 }
