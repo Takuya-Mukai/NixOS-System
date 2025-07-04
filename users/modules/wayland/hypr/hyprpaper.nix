91268784c324ce99ea619453b1e389da0fdc91c9
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-04 19:58:16 +0900

    path

--- users/modules/wayland/hypr/hyprpaper.nix
+++ users/modules/wayland/hypr/hyprpaper.nix
@@ -7,14 +7,14 @@
 
       # 事前に読み込む壁紙
       preload = [
-        "${../modules/config/hypr/backgrounds/Small-memory-sunrise.jpg}"
+        "${../../config/hypr/backgrounds/Small-memory-sunrise.jpg}"
       ];
 
       # モニターに壁紙を割り当て
       wallpaper = [
         # "DP-1,/path/to/wallpaper.png" の形式
         # モニター名を空にすると、全てのモニターに適用される
-        ",${../modules/config/hypr/backgrounds/Small-memory-sunrise.jpg}"
+        ",${../../config/hypr/backgrounds/Small-memory-sunrise.jpg}"
       ];
     };
   };
