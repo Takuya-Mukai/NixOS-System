110b8ca4bf81f4d4e7d54877ef463cea12e80edc
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-06 03:13:06 +0900

    changed to auto

--- modules/wayland/hypr/monitor/hyprland-monitor-mini.nix
+++ modules/wayland/hypr/monitor/hyprland-monitor-mini.nix
@@ -2,10 +2,10 @@
   wayland.windowManager.hyprland.settings = {
     # モニターの物理的な設定
     monitor = [
-      "DP-1, 1920x1080@60, 1"
-      "DP-2, 1920x1080@60, 0x0, 1"
-      "HDMI-A-1, 1920x1080@60, 1"
-      "HDMI-A-2, 1920x1080@60, 1"
+      "DP-1, 1920x1080@60, auto, 1"
+      "DP-2, 1920x1080@60, auto, 1"
+      "HDMI-A-1, 1920x1080@60, auto, 1"
+      "HDMI-A-2, 1920x1080@60, auto, 1"
     ];
   };
 }
