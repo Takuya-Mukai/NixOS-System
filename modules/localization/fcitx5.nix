0144cc41af20ed917e81433ad8135d96756c6ab0
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-05 16:49:47 +0900

    removed

--- modules/localization/fcitx5.nix
+++ modules/localization/fcitx5.nix
@@ -23,9 +23,9 @@
     ".local/share/fcitx5/skk/dictionary_list".text = with pkgs; ''
       file=${libskk}/share/skk/SKK-JISYO.L,mode=readonly,type=file
     '';
-    ".config/fcitx5" = {
-      source = ./fcitx5;
-      recursive = true;
-    };
+    # ".config/fcitx5" = {
+    #   source = ./fcitx5;
+    #   recursive = true;
+    # };
   };
 }
