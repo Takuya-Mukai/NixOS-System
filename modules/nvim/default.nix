10886f2134c4f333dcc2a67c621e72b572a1647c
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-06 19:10:23 +0900

    m

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,34 +1,41 @@
 { config, pkgs, ... }:
 
 let
-    toVarName = str: builtins.replaceStrings ["-"] ["_"] str;
-  
-    buildSubstArgs = pkgs: plugins: builtins.listToAttrs (builtins.map (p: {
-      name = toVarName p;
-      value = builtins.getAttr p pkgs.vimPlugins;
-    }) plugins);
-  
-    pluginNames = [
-    # cmp
-    "nvim-cmp" "cmp-nvim-lsp" "cmp-buffer" "cmp-path" "cmp-cmdline" "lspkind-nvim" "copilot-lua" "luasnip" "cmp_luasnip" "friendly-snippets" "copilot-cmp" "lspkind-nvim" "cmp-latex-symbols"
-    ];
-  
-in
+  pluginNames = [
+    "nvim-cmp" "cmp-nvim-lsp" "cmp-buffer" "cmp-path"
+    "cmp-cmdline" "lspkind-nvim" "copilot-lua" "luasnip"
+    "cmp_luasnip" "friendly-snippets" "copilot-cmp"
+    "cmp-latex-symbols"
+  ];
 
-{
+  toVarName = str: builtins.replaceStrings ["-"] ["_"] str;
+
+  pluginVars = builtins.listToAttrs (builtins.map (p: {
+    name = toVarName p;
+    value = pkgs.vimPlugins.${p};
+  }) pluginNames);
+
+  luaFiles = builtins.filter (file: builtins.match ".*\\.lua$" file != null)
+    (builtins.attrNames (builtins.readDir ./config/nvim/lua/plugins));
+
+  replacedLuaFiles = builtins.listToAttrs (map (file: {
+    name = ".config/nvim/lua/plugins/${file}";
+    value = pkgs.substituteAll ({
+      src = ./config/nvim/lua/plugins/${file};
+      name = file;
+    } // pluginVars);
+  }) luaFiles);
+
+in {
   programs.neovim = {
-    enable = true; # nvimパッケージもインストールされる
+    enable = true;
     extraPackages = with pkgs; [
-      # dependencies for neovim
       gnumake
       gcc
       nodePackages.nodejs
-    ] ++ (map (p: builtins.getAttr p pkgs.vimPlugins) pluginNames);
+    ] ++ (map (p: pkgs.vimPlugins.${p}) pluginNames);
   };
-  home.file.".config/nvim" = {
-    source = pkgs.substituteAllFiles ({
-      src = ../config/nvim;
-    } // buildSubstArgs pkgs pluginNames);
-    recursive = true;
-  };
+
+  home.file = replacedLuaFiles;
 }
+
