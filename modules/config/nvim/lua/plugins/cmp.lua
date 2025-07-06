5c0b420f72705f9d99c5e86c568484c1942f264f
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-06 18:56:10 +0900

    modified

--- modules/config/nvim/lua/plugins/cmp.lua
+++ modules/config/nvim/lua/plugins/cmp.lua
@@ -1,38 +1,82 @@
 return{
   {
---    "hrsh7th/nvim-cmp",
+    -- "hrsh7th/nvim-cmp",
     name = "nvim-cmp",
     dir = "@nvim_cmp@",
     event = { "InsertEnter", "CmdlineEnter" },
     config = function() require 'plugins-config.cmp.nvim-cmp' end,
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
+      -- "hrsh7th/cmp-nvim-lsp",
+      name = "cmp-nvim-lsp",
+      dir = "@cmp_nvim_lsp@",
       event = { "InsertEnter", "CmdlineEnter"},
       dependencies = {
         {
-          'L3MON4D3/LuaSnip',
-          build = 'make install_jsregexp',
+          -- 'L3MON4D3/LuaSnip',
+          name = "LuaSnip",
+          dir = "@luasnip@",
+          -- build = 'make install_jsregexp',
           config = function() require 'plugins-config.cmp.luasnip' end,
-          dependencies = { 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets' },
+          dependencies = {
+            {
+              -- 'saadparwaiz1/cmp_luasnip'
+              name = "cmp_luasnip",
+              dir = "@cmp_luasnip@"
+            },
+            {
+              -- 'rafamadriz/friendly-snippets'
+              name = "friendly-snippets",
+              dir = "@friendly_snippets@",
+            },
+          },
           event = "InsertEnter",
         },
         {
-          'zbirenbaum/copilot-cmp',
-          dependencies = 'copilot.lua',
+          -- 'zbirenbaum/copilot-cmp',
+          name = "copilot-cmp",
+          dir = "@copilot_cmp@",
+          dependencies =
+          {
+            -- 'copilot.lua',
+            name = "copilot.lua",
+            dir = "@copilot_lua@",
+          },
           config = function() require('copilot_cmp').setup() end,
           cmd = "CS",
           event = "InsertEnter",
         },
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'onsails/lspkind.nvim',
-        "kdheepak/cmp-latex-symbols",
+        {
+          -- 'hrsh7th/cmp-buffer',
+          name = "cmp-buffer",
+          dir = "@cmp_buffer@",
+        },
+        {
+          -- 'hrsh7th/cmp-path',
+          name = "cmp-path",
+          dir = "@cmp_path@",
+        },
+        {
+          -- 'hrsh7th/cmp-cmdline',
+          name = "cmp-cmdline",
+          dir = "@cmp_cmdline@",
+        },
+        {
+          -- 'onsails/lspkind.nvim',
+          name = "lspkind.nvim",
+          dir = "@lspkind_nvim@",
+        },
+        {
+          -- "kdheepak/cmp-latex-symbols",
+          name = "cmp-latex-symbols",
+          dir = "@cmp_latex_symbols",
+        },
       },
     }
   },
   {
-    "zbirenbaum/copilot.lua",
+    -- "zbirenbaum/copilot.lua",
+    name = "copilot.lua",
+    dir = "copilot_lua",
     event = "InsertEnter",
     config = function()
       require("plugins-config.cmp.copilot")
--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,5 +1,21 @@
 { config, pkgs, ... }
 {
+
+  let
+    toVarName = str: builtins.replaceStrings ["-"] ["_"] str;
+  
+    buildSubstArgs = pkgs: plugins: builtins.listToAttrs (builtins.map (p: {
+      name = toVarName p;
+      value = builtins.getAttr p pkgs.vimPlugins;
+    }) plugins);
+  
+    pluginNames = [
+    # cmp
+    "nvim-cmp" "cmp-nvim-lsp" "cmp-buffer" "cmp-path" "cmp-cmdline" "lspkind-nvim" "copilot-lua" "luasnip" "cmp_luasnip" "friendly-snippets" "copilot-cmp" "lspkind-nvim" "cmp-latex-symbols"
+    ];
+  
+  in
+
   programs.neovim = {
     enable = true; # nvimパッケージもインストールされる
     extraPackages = with pkgs; [
@@ -7,12 +23,13 @@
       gnumake
       gcc
       nodePackages.nodejs
-
-      # plugins
-      with vimPlugins; [
-        nvim-cmp
-      ];
-    ];
+    ] ++ (map (p: builtins.getAttr p pkgs.vimPlugins) pluginNames);
   };
-  home.file.".config/nvim".source = ../config/nvim;
+  home.file.".config/nvim" = {
+    source = pkgs.substituteAllFiles ({
+      src = ../config/nvim;
+    } // buildSubstArgs pkgs pluginNames);
+    recursive = true;
+  };
+
 }
