dc62e2a53f38c8e0c8abfae69574edd840428f45
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-10 01:42:25 +0900

    disabled ipv6

--- system/core.nix
+++ system/core.nix
@@ -15,14 +15,13 @@
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowBroken = true;
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.enableIPv6 = false;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
