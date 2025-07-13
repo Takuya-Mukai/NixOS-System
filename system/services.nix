9397e38e4d11ae39bf204906579632fe11def81a
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-13 22:49:22 +0900

    enabled nftables

--- system/services.nix
+++ system/services.nix
@@ -4,10 +4,13 @@
     blueman.enable = true;
     tailscale.enable = true;
   };
-  networking.firewall = {
-    enable = true;
-    trustedInterfaces = ["tailscale0"];
-    allowedUDPPorts = [config.services.tailscale.port];
-    allowedTCPPorts = [ 22 ];
+  networking = {
+    firewall = {
+      enable = true;
+      trustedInterfaces = ["tailscale0"];
+      allowedUDPPorts = [config.services.tailscale.port];
+      allowedTCPPorts = [ 22 ];
+    };
+    nftables.enable = true;
   };
 }
