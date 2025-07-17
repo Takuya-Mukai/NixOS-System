5948fca693841d22cc33296986a9b2d40968999f
Takuya-Mukai <takuya.mukai@gmail.com>
2025-07-18 02:23:46 +0900

    modified legacy setting

--- system/sound.nix
+++ system/sound.nix
@@ -1,19 +1,21 @@
 { config, pkgs, ... }:
 {
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    jack.enable = true;
-    pulse.enable = true;
-    wireplumber.enable = true;
-    extraConfig = {
-      pipewire."99-silent-bell.conf" = {
-        "context.properties" = {
-          "module.x11.bell" = false;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+      extraConfig = {
+        pipewire."99-silent-bell.conf" = {
+          "context.properties" = {
+            "module.x11.bell" = false;
+          };
         };
       };
     };
