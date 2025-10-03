5e9e0ac46b17c4eac0f1956d7e72ac588bb0647c
Takuya-Mukai <takuya.mukai@gmail.com>
2025-10-03 23:37:28 +0900

    Update: Flatpak added

--- system/services.nix
+++ system/services.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   services = {
+    flatpak.enable = true;
     blueman.enable = true;
     tailscale.enable = true;
     openssh = {
@@ -14,18 +15,27 @@
   networking = {
     firewall = {
       enable = true;
-      trustedInterfaces = ["tailscale0" "virbr0"];
-      allowedUDPPorts = [config.services.tailscale.port];
+      trustedInterfaces = [
+        "tailscale0"
+        "virbr0"
+      ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
       allowedTCPPorts = [ 22 ];
       # option for tailscale exitnode
       checkReversePath = "loose";
       # port for kde connect
       allowedTCPPortRanges = [
-      	{ from = 1714; to = 1764; }
-      	# { from = 37000; to = 44000; }
+        {
+          from = 1714;
+          to = 1764;
+        }
+        # { from = 37000; to = 44000; }
       ];
       allowedUDPPortRanges = [
-      	{ from = 1714; to = 1764; }
+        {
+          from = 1714;
+          to = 1764;
+        }
       ];
     };
     nftables.enable = true;
